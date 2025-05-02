574be138fbb512491378b249313f9dd4e5146691
van <vexyzy@yandex.ru>
2025-05-02 14:47:25 +0300

    Add db-connection settings

--- app/core/config.py
+++ app/core/config.py
@@ -24,6 +24,26 @@ class AuthSettings(ModelConfig):
     access_token_expire_minutes: int = Field(default=30, validation_alias="APP_ACCESS_TOKEN_EXPIRE_MINUTES")
 
 
+class DBSettings(ModelConfig):
+    db_type: str = Field(default="postgresql", validation_alias="DB_TYPE")
+    db_host: str = Field(default="localhost", validation_alias="DB_HOST")
+    db_port: str = Field(default="5432", validation_alias="DB_PORT")
+    db_driver: str = Field(default="asyncpg", validation_alias="DB_DRIVER")
+    db_user: str = Field(default="root", validation_alias="DB_USER")
+    db_user_password: str = Field(default="root", validation_alias="DB_USER_PASSWORD")
+    db_name: str = Field(default="db", validation_alias="DB_NAME")
+    db_echo: bool = Field(default=False, validation_alias="DB_ECHO")
+
+    @property
+    def db_url(self):
+        return (
+            f"{self.db_type}+{self.db_driver}://"
+            f"{self.db_user}:{self.db_user_password}@"
+            f"{self.db_host}:{self.db_port}/{self.db_name}"
+        )
+
+
 # create config instances
 host_settings = HostSettings()
 auth_settings = AuthSettings()
+db_settings = DBSettings()
---
+++ app/core/postgres.py
@@ -0,0 +1,33 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from core.config import db_settings
+
+DATABASE_URL = db_settings.db_url
+ECHO = db_settings.db_echo
+
+
+class DataBaseHelper:
+    def __init__(self, db_url: str, echo: bool):
+        self.__engine: AsyncEngine = create_async_engine(db_url, echo=echo)
+        self.__session_factory = async_sessionmaker(
+            bind=self.__engine,
+            expire_on_commit=False,
+            autoflush=False,
+            autocommit=False,
+        )
+
+    @asynccontextmanager
+    async def session_dependency(self):
+        async with self.__session_factory() as session:
+            yield session
+            await session.close()
+
+
+db_helper = DataBaseHelper(DATABASE_URL, ECHO)
