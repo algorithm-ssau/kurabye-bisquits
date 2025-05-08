beecf213f0375b5d37ccbb1d572cd922322f9f7e
Denis Motyacow <motakovd@gmail.com>
2025-05-08 13:52:23 +0300

    Reformat product schemas
    If proteins or fats or carbohydrates doesn't specified, we set im to the
    0, not for None

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -16,12 +16,12 @@ class ProductFullResponseSchema(ProductResponseSchema):
     description: str | None = Field(default=None, description="The information about the project.")
     composition: list = Field(description="The composition of the product, e.g.: milk, sugar and etc.")
     energy: int = Field(description="The energy of the product of the 100 grams, e.g. 100kCal")
-    fats: int | None = Field(default=None)
-    carbohydrates: int | None = Field(default=None)
-    proteins: int | None = Field(default=None)
+    fats: int = Field(default=0)
+    carbohydrates: int = Field(default=0)
+    proteins: int = Field(default=0)
 
 
-class ProductQueryParams(BaseModel):
+class ProductListQueryParams(BaseModel):
     limit: int = Field(default=12, gt=0, le=25, description="Limit of the products on the page.")
     offset: int = Field(default=0, ge=0, description="Offset of the for products for the pagination.")
     sorting_by: str | None = Field(default=None, description="The rule of sorting the elements.")
@@ -30,3 +30,8 @@ class ProductQueryParams(BaseModel):
         default="asc",
         description="The sorting order, asc — from the least to the gretest, desc — from the greatest to the least.",
     )
+
+
+class ProductQueryParams(BaseModel):
+    product_id: int
+    package_id: int
