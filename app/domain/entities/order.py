517029ab61b422320cc0f285e91e43c729d0c80b
Denis Motyacow <motakovd@gmail.com>
2025-05-08 21:09:24 +0300

    Add UpdateOrder model

--- app/domain/entities/order.py
+++ app/domain/entities/order.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 
 class Order(BaseModel):
@@ -7,3 +7,15 @@ class Order(BaseModel):
     user_id: int
     shipping_adderess: str
     comment: str | None = None
+
+
+class UpdateOrderData(BaseModel):
+    status_id: int | None = None
+    shipping_adderess: str | None = None
+    comment: str | None = None
+
+    @model_validator(mode="after")
+    def check_at_least_one_field_seted(self):
+        if not any((self.status_id, self.shipping_adderess, self.comment)):
+            raise ValueError("At least one value should be setted for update")
+        return self
