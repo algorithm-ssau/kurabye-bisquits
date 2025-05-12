3b043a7b6865e1f65671b19d65319fac498d33ff
Andrey <emeralderm20@gmail.com>
2025-05-12 16:21:20 +0300

    Add new sql-queries

--- app/repository/sql/cartQueries.py
+++ app/repository/sql/cartQueries.py
@@ -25,8 +25,6 @@ SELECT_CART_ITEMS = text(
     from cart
     join cart_item using(cart_id)
     join product using(product_id)
-    join product_to_package using(product_id)
-    join package using(package_id)
     where cart_id = :cart_id
     group by cart_id;
     """
@@ -37,7 +35,8 @@ UPSERT_PRODUCT_IN_CART = text(
     insert into cart_item (cart_id, product_id, quantity)
     values (:cart_id, :product_id, :quantity)
     on conflict (cart_id, product_id)
-    do update set quantity = cart_item.quantity + excluded.quantity;
+    do update set quantity = cart_item.quantity + excluded.quantity
+    returning quantity;
     """
 )
 
@@ -59,5 +58,16 @@ CREATE_CART = text(
     """
     insert into cart(cart_id) values(:cart_id)
     on conflict(cart_id) do nothing;
+    """
+)
+
+
+GET_TOTAL_PRODUCT_QUANTITY = text(
+    """
+    select
+    	sum(stock_quantity) as total_quantity
+    from inventory
+    where product_id = :product_id
+    group by product_id;
     """
 )
